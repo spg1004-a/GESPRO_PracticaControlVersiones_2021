799fb125b0bade277f06138f392b9af35bdfe535
spg1004-a <sergiop9989@gmail.com>
2020-12-24 20:16:47 +0100

    Add Crashlytics #195

--- app/src/main/java/com/davidmiguel/gobees/logging/Log.java
+++ app/src/main/java/com/davidmiguel/gobees/logging/Log.java
@@ -18,6 +18,7 @@
 
 package com.davidmiguel.gobees.logging;
 
+import com.crashlytics.android.Crashlytics;
 import com.davidmiguel.gobees.BuildConfig;
 
 import timber.log.Timber;
@@ -40,8 +41,29 @@ public class Log {
         if (BuildConfig.DEBUG) {
             Timber.plant(new DebugTree());
         }
+		 // Log to Crashlytics
+        Timber.plant(new CrashlyticsTree());
+        // Attach build info
+        setCommitHash(BuildConfig.COMMIT_HASH);
+        setBuildDate(BuildConfig.BUILD_DATE);
     }
 
+    /**
+     * Attaches commit hash in crash reports.
+     */
+    public static void setCommitHash(String hash) {
+        Crashlytics.setString("commit_hash", hash);
+    }
+
+    /**
+     * Attaches build date in crash reports.
+     */
+    public static void setBuildDate(String date) {
+        Crashlytics.setString("build_date", date);
+    
+    }
+	
+	
     /**
      * Log a verbose message with optional format args.
      */
